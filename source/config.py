1310245a6b0c4706d248c38ae3e8d93c9322dc25
borisovodov <b.ovodov@mail.ru>
2015-05-24 18:56:50 +0500

    Print about create config file.

--- source/config.py
+++ source/config.py
@@ -5,7 +5,7 @@ from source.func import PATH
 
 
 def create_config():
-    key_google = input('Input Google API key: ')
+    key_google = input('Create config file.\nInput Google API key: ')
     key_flickr = input('Input Flickr Key: ')
     key_flickr_secret = input('Input Flickr Secret: ')
     content = 'keys = {\'google\': \'' + key_google + '\', \'flickr\': \'' + key_flickr + '\', \'flickr_secret\': \''\
@@ -13,6 +13,7 @@ def create_config():
     file_config = open(PATH + '/data/settings.py', encoding='utf-8', mode='w')
     file_config.write(content)
     file_config.close()
+    print('Config file created.')
 
 if os.path.isfile(PATH + '/data/settings.py') == False:
     create_config()
