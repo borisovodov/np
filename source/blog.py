c77477121d71efb2af353675cfbeb1b23c928496
borisovodov <b.ovodov@gmail.com>
2015-06-03 14:15:04 +0500

    Work with Storage.

--- source/blog.py
+++ source/blog.py
@@ -3,6 +3,7 @@
 import os
 import pip
 import pprint
+from np import PATH
 from source.config import keys, ids
 
 KEY_GOOGLE_CLIENT_ID = keys['google_client_id']
@@ -23,34 +24,39 @@ def authorization_blogger():
     import httplib2
     import webbrowser
     from oauth2client.client import OAuth2WebServerFlow
+    from oauth2client.file import Storage
     from googleapiclient.discovery import build
 
     print('Authorization on Blogger...')
     try:
+        storage = Storage(PATH + '/data/storage.db')
+        credentials = storage.get()
+
+        http_auth = credentials.authorize(httplib2.Http())
+        blog = build('blogger', 'v3', http=http_auth)
+    except:
         flow = OAuth2WebServerFlow(client_id=KEY_GOOGLE_CLIENT_ID,
                                    client_secret=KEY_GOOGLE_CLIENT_SECRET,
                                    scope='https://www.googleapis.com/auth/blogger',
                                    redirect_uri='urn:ietf:wg:oauth:2.0:oob')
         auth_uri = flow.step1_get_authorize_url()
         webbrowser.open(auth_uri)
-        code = input('Input code: ')
-        credentials = flow.step2_exchange(code)
+        auth_code = input('Enter the auth code: ')
+        credentials = flow.step2_exchange(auth_code)
 
-        http = httplib2.Http()
-        http = credentials.authorize(http)
-        blog = build('blogger', 'v3', http=http)
+        storage = Storage(PATH + '/data/storage.db')
+        storage.put(credentials)
 
-        print('Complete authorization.')
-        return blog
-    except:
-        print('Anything wrong with authorization on Blogger. Try again later.')
-        return False
+        http_auth = credentials.authorize(httplib2.Http())
+        blog = build('blogger', 'v3', http=http_auth)
+    print('Complete authorization.')
+    return blog
 
 
-def update_page(blog, page):
+def update_page(blog, page, content):
     pages = blog.pages().list(blogId=ID_BLOGGER_BLOG).execute()
     pprint.pprint(pages)
 
-#post = blogger.posts().insert(blogId=ID_BLOGGER_BLOG, body='')
-#resp = post.execute()
-#pprint.pprint(resp)
+blog = authorization_blogger()
+pages = blog.pages().list(blogId=ID_BLOGGER_BLOG).execute()
+pprint.pprint(pages)
