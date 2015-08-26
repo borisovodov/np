1c838463c73c5b598826c1fcdf219e915f22e7ed
borisovodov <b.ovodov@gmail.com>
2015-08-26 13:51:10 +0500

    Export module complete.

--- src/export.py
+++ src/export.py
@@ -4,7 +4,7 @@ import sys
 import xml.etree.ElementTree as ET
 from .newspaper import newspapers
 
-EXPORT_PATH = sys.path[0] + '/tmp/newspapers.xml'
+EXPORT_PATH = sys.path[0] + '/newspapers.xml'
 
 
 def export():
@@ -41,6 +41,47 @@ def export():
         coordinates_element = ET.SubElement(newspaper_element, 'coordinates')
         ET.SubElement(coordinates_element, 'latitude').text = str(newspaper.coordinates.latitude)
         ET.SubElement(coordinates_element, 'longitude').text = str(newspaper.coordinates.longitude)
+        ET.SubElement(newspaper_element, 'date brought').text = str(newspaper.date_brought.day) + '.'\
+                                                                + str(newspaper.date_brought.month) + '.'\
+                                                                + str(newspaper.date_brought.year)
+        ET.SubElement(newspaper_element, 'color').text = newspaper.color
+        ET.SubElement(newspaper_element, 'pages').text = str(newspaper.pages)
+        format_element = ET.SubElement(newspaper_element, 'format', id=str(newspaper.format_paper))
+        ET.SubElement(format_element, 'name').text = newspaper.format_paper.name
+        ET.SubElement(format_element, 'height').text = newspaper.format_paper.height
+        ET.SubElement(format_element, 'width').text = newspaper.format_paper.width
+        ET.SubElement(newspaper_element, 'type').text = newspaper.type
+        costs_element = ET.SubElement(newspaper_element, 'costs')
+        for cost in newspaper.costs:
+            cost_element = ET.SubElement(costs_element, 'cost')
+            ET.SubElement(cost_element, 'value').text = str(cost.value)
+            currency_element = ET.SubElement(cost_element, 'currency', id=str(cost.currency.id))
+            ET.SubElement(currency_element, 'name').text = cost.currency.name
+            ET.SubElement(currency_element, 'symbol').text = cost.currency.symbol
+        ET.SubElement(newspaper_element, 'frequency').text = newspaper.frequency
+        ET.SubElement(newspaper_element, 'circulation').text = str(newspaper.circulation)
+        ET.SubElement(newspaper_element, 'site').text = newspaper.site
+        ET.SubElement(newspaper_element, 'issn').text = newspaper.issn
+        ET.SubElement(newspaper_element, 'date start publication').text = str(newspaper.date_start_publication.day) + '.'\
+                                                                          + str(newspaper.date_start_publication.month) + '.'\
+                                                                          + str(newspaper.date_start_publication.year)
+        ET.SubElement(newspaper_element, 'geotag').text = str(newspaper.geotag)
+        ET.SubElement(newspaper_element, 'crossword').text = str(newspaper.crossword)
+        ET.SubElement(newspaper_element, 'sudoku').text = str(newspaper.sudoku)
+        ET.SubElement(newspaper_element, 'nonogram').text = str(newspaper.nonogram)
+        ET.SubElement(newspaper_element, 'kakuro').text = str(newspaper.kakuro)
+        ET.SubElement(newspaper_element, 'ad_toyota').text = str(newspaper.ad_toyota)
+        ET.SubElement(newspaper_element, 'tv_schedule').text = str(newspaper.tv_schedule)
+        ET.SubElement(newspaper_element, 'anecdote').text = str(newspaper.anecdote)
+        ET.SubElement(newspaper_element, 'caricature').text = str(newspaper.caricature)
+        ET.SubElement(newspaper_element, 'comic_strip').text = str(newspaper.comic_strip)
+        ET.SubElement(newspaper_element, 'recipe').text = str(newspaper.recipe)
+        ET.SubElement(newspaper_element, 'horoscope').text = str(newspaper.horoscope)
+        ET.SubElement(newspaper_element, 'weather_forecast').text = str(newspaper.weather_forecast)
+        ET.SubElement(newspaper_element, 'naked_women').text = str(newspaper.naked_women)
+        ET.SubElement(newspaper_element, 'church').text = str(newspaper.church)
+        ET.SubElement(newspaper_element, 'trash').text = str(newspaper.trash)
+        ET.SubElement(newspaper_element, 'url').text = newspaper.url
 
     ET.ElementTree(root).write(EXPORT_PATH, encoding='UTF-8', xml_declaration=True)
     print('Export file created.')
