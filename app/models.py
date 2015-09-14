0a2e0930e9095b0db564f89e94b5d1cc807689e1
borisovodov <b.ovodov@mail.ru>
2015-09-14 22:47:29 +0500

    Django in process.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,41 @@
 """Module contain all classes."""
 
+from math import trunc
 from django.db import models
+from src.config import config
+
+DRIVE_FOLDER_MARKER_ID = config('drive_folder_marker_id')
+
+
+class Coordinates(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def format(self, coord):
+        if self.latitude > 0:
+            lat = str(trunc(float(self.latitude))) + '° ' + str(trunc((float(self.latitude) -
+                                                                       trunc(float(self.latitude)))*60)) + '′ N'
+        else:
+            lat = str(abs(trunc(float(self.latitude)))) + '° ' + str(abs(trunc((float(self.latitude) -
+                                                                                trunc(float(self.latitude)))*60)))\
+                                                               + '′ S'
+        if self.longitude > 0:
+            long = str(trunc(float(self.longitude))) + '° ' + str(trunc((float(self.longitude) -
+                                                                         trunc(float(self.longitude)))*60)) + '′ E'
+        else:
+            long = str(abs(trunc(float(self.longitude)))) + '° ' + str(abs(trunc((float(self.longitude) -
+                                                                                  trunc(float(self.longitude)))*60)))\
+                                                                 + '′ W'
+
+        if coord == 'latitude':
+            return lat
+        elif coord == 'longitude':
+            return long
+        elif coord == 'both':
+            return {'latitude': lat, 'longitude': long}
+
+    def __str__(self):
+        return self.format('latitude') + ' ' + self.format('longitude')
 
 
 class Currency(models.Model):
@@ -20,8 +55,43 @@ class FormatPaper(models.Model):
         return self.name + ' (' + str(self.height) + '×' + str(self.width) + ')'
 
 
+class Hemisphere(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    name = models.CharField()
+    population = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class Sender(models.Model):
     name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+
+class Cost(models.Model):
+    value = 0.0
+    currency = models.ForeignKey(Currency)
+
+    def __str__(self):
+        return str(self.value) + ' ' + str(self.currency)
+
+
+class Country(models.Model):
+    name = models.CharField()
+    languages = models.ManyToManyField(Language)
+    population = models.IntegerField()
+
+    def marker(self):
+        return 'https://googledrive.com/host/' + DRIVE_FOLDER_MARKER_ID + '/' + self.name.lower() + '.png'
 
     def __str__(self):
         return self.name
