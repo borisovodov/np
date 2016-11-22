a8b5a45c7813168742a2903f869dbad778e12d7c
borisovodov <b.ovodov@gmail.com>
2016-11-22 22:53:48 +0500

    Remove buissness logic start and exit from app.

--- np.py
+++ np.py
@@ -19,18 +19,5 @@ if not os.path.isdir(os.path.join(sys.path[0], 'tmp')):
 setup()
 
 while True:
-    menu = input('\nWhat you want?\nStart app\nMigration\nSuperuser\nUpdate app\nExit\n').lower()
-    if menu == 'start' or menu == 's' or menu == 'start app':
-        webbrowser.open('http://127.0.0.1:1000/admin/')
-        os.system('python manage.py runserver 1000')
-    elif menu == 'migration' or menu == 'm':
-        os.system('python manage.py makemigrations')
-        os.system('python manage.py migrate')
-    elif menu == 'superuser':
-        os.system('python manage.py createsuperuser')
-    elif menu == 'update' or menu == 'u' or menu == 'update app':
-        update()
-    elif menu == 'exit' or menu == 'e':
-        break
-    else:
-        print('Wrong! Please, input again.')
+    webbrowser.open('http://127.0.0.1:1000/admin/')
+    os.system('python manage.py runserver 1000')
