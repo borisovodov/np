47a51545eae3610ab0204e8626aad84e98e4f6da
borisovodov <boris@ovodov.me>
2023-12-24 13:08:08 +0500

    ...

--- functions/hello.js
+++ functions/hello.js
@@ -1,16 +1,12 @@
 export async function onRequest(context) {
-	async function getAllNewspapers(env) {
-  		return await env.db.prepare('SELECT * FROM newspaper').all()
-	}
-
 	//async function getFilesList(env) {
 	//  return await env.bucket.list()
 	//}
 
-	let allNewspapers = await getAllNewspapers(env)
+	let allNewspapers = await context.env.db.prepare('SELECT * FROM newspaper').all()
 
-	//let text = {"results": allNewspapers}
+	let text = {"results": allNewspapers}
 
-  //return new Response(JSON.stringify(text))
-	return new Response("Helllllooooooo!!")
+  return new Response(JSON.stringify(text))
+	//return new Response("Helllllooooooo!!")
 }
