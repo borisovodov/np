c913d39c92b6760eafc1b86c55dece4474df78af
borisovodov <boris@ovodov.me>
2023-12-31 12:35:02 +0500

    ...

--- functions/hello.js
+++ functions/hello.js
@@ -2,7 +2,7 @@ export async function onRequest(context) {
 	let allNewspapers = await context.env.db.prepare('SELECT * FROM newspaper').all()
 	let files = await context.env.bucket.list()
 
-	let text = {"results": allNewspapers,
+	let text = {"newspapers": allNewspapers,
 				"files": files.objects,}
 
   return new Response(JSON.stringify(text), {
